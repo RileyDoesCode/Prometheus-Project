21618ff27ef66a3c25a408baceb79adb00a463a7
RileyDoesCode <enriquejoaquingreyes11@gmail.com>
2026-03-06 16:34:12 +0800

    Update Il.lua

--- Prometheus/src/prometheus/namegenerators/Il.lua
+++ Prometheus/src/prometheus/namegenerators/Il.lua
@@ -2,40 +2,45 @@
 --
 -- namegenerators/il.lua
 --
--- This Script provides a function for generation of weird names consisting of I, l and 1
+-- This Script provides a function for the generation of weird names consisting of I, l, and 1
 
-local MIN_CHARACTERS = 5;
-local MAX_INITIAL_CHARACTERS = 10;
+local util = require("prometheus.util")
+local chararray = util.chararray
 
+local MIN_CHARACTERS = 5
+local MAX_INITIAL_CHARACTERS = 10
 
-local util = require("prometheus.util");
-local chararray = util.chararray;
-
-local offset = 0;
-local VarDigits = chararray("Il1");
-local VarStartDigits = chararray("Il");
+local offset = 0
+local VarDigits = chararray("Il1")
+local VarStartDigits = chararray("Il")
 
 local function generateName(id, scope)
-	local name = ''
-	id = id + offset;
-	local d = id % #VarStartDigits
-	id = (id - d) / #VarStartDigits
-	name = name..VarStartDigits[d+1]
+	id = id + offset
+	
+	local startLen = #VarStartDigits
+	local d = id % startLen
+	id = (id - d) / startLen
+	
+	-- Using a table for faster iterative string building
+	local result = { VarStartDigits[d + 1] }
+	
+	local digitsLen = #VarDigits
 	while id > 0 do
-		local d = id % #VarDigits
-		id = (id - d) / #VarDigits
-		name = name..VarDigits[d+1]
+		d = id % digitsLen
+		id = (id - d) / digitsLen
+		result[#result + 1] = VarDigits[d + 1]
 	end
-	return name
+	
+	return table.concat(result)
 end
 
 local function prepare(ast)
-	util.shuffle(VarDigits);
-	util.shuffle(VarStartDigits);
-	offset = math.random(3 ^ MIN_CHARACTERS, 3 ^ MAX_INITIAL_CHARACTERS);
+	util.shuffle(VarDigits)
+	util.shuffle(VarStartDigits)
+	offset = math.random(3 ^ MIN_CHARACTERS, 3 ^ MAX_INITIAL_CHARACTERS)
 end
 
 return {
 	generateName = generateName, 
 	prepare = prepare
-};
+}
