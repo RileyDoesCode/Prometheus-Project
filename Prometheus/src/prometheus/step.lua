8bd914435b4f389ab71377bc7e50beae963903f0
RileyDoesCode <enriquejoaquingreyes11@gmail.com>
2026-03-06 15:40:15 +0800

    Update step.lua

--- Prometheus/src/prometheus/step.lua
+++ Prometheus/src/prometheus/step.lua
@@ -4,76 +4,73 @@
 --
 -- This file Provides the base class for Obfuscation Steps
 
-local logger = require("logger");
-local util = require("prometheus.util");
+local logger = require("logger")
+local util = require("prometheus.util")
 
-local lookupify = util.lookupify;
+local lookupify = util.lookupify
 
-local Step = {};
+local Step = {}
 
 Step.SettingsDescriptor = {}
+Step.Name = "Abstract Step"
+Step.Description = "Abstract Step"
 
 function Step:new(settings)
-	local instance = {};
-	setmetatable(instance, self);
-	self.__index = self;
+	local instance = {}
+	setmetatable(instance, self)
+	self.__index = self
 	
-	if type(settings) ~= "table" then
-		settings = {};
-	end
+	settings = type(settings) == "table" and settings or {}
 	
 	for key, data in pairs(self.SettingsDescriptor) do
-		if settings[key] == nil then
+		local val = settings[key]
+		
+		if val == nil then
 			if data.default == nil then
-				logger:error(string.format("The Setting \"%s\" was not provided for the Step \"%s\"", key, self.Name));
+				logger:error(string.format("The Setting \"%s\" was not provided for the Step \"%s\"", key, self.Name))
 			end
-			instance[key] = data.default;
-		elseif(data.type == "enum") then
-			local lookup = lookupify(data.values);
-			if not lookup[settings[key]] then
-				logger:error(string.format("Invalid value for the Setting \"%s\" of the Step \"%s\". It must be one of the following: %s", key, self.Name, table.concat(data, ", ")));
-			end
-			instance[key] = settings[key];
-		elseif(type(settings[key]) ~= data.type) then
-			logger:error(string.format("Invalid value for the Setting \"%s\" of the Step \"%s\". It must be a %s", key, self.Name, data.type));
+			instance[key] = data.default
 		else
-			if data.min then
-				if  settings[key] < data.min then
-					logger:error(string.format("Invalid value for the Setting \"%s\" of the Step \"%s\". It must be at least %d", key, self.Name, data.min));
+			if data.type == "enum" then
+				local lookup = lookupify(data.values)
+				if not lookup[val] then
+					logger:error(string.format("Invalid value for the Setting \"%s\" of the Step \"%s\". It must be one of the following: %s", key, self.Name, table.concat(data.values, ", ")))
 				end
-			end
-			
-			if data.max then
-				if  settings[key] > data.max then
-					logger:error(string.format("Invalid value for the Setting \"%s\" of the Step \"%s\". The biggest allowed value is %d", key, self.Name, data.min));
+				instance[key] = val
+			elseif type(val) ~= data.type then
+				logger:error(string.format("Invalid value for the Setting \"%s\" of the Step \"%s\". It must be a %s", key, self.Name, data.type))
+			else
+				if data.min and val < data.min then
+					logger:error(string.format("Invalid value for the Setting \"%s\" of the Step \"%s\". It must be at least %d", key, self.Name, data.min))
 				end
+				
+				if data.max and val > data.max then
+					logger:error(string.format("Invalid value for the Setting \"%s\" of the Step \"%s\". The biggest allowed value is %d", key, self.Name, data.max))
+				end
+				
+				instance[key] = val
 			end
-			
-			instance[key] = settings[key];
 		end
 	end
 	
-	instance:init();
+	instance:init()
 
-	return instance;
+	return instance
 end
 
 function Step:init()
-	logger:error("Abstract Steps cannot be Created");
+	logger:error("Abstract Steps cannot be Created")
 end
 
 function Step:extend()
-	local ext = {};
-	setmetatable(ext, self);
-	self.__index = self;
-	return ext;
+	local ext = {}
+	setmetatable(ext, self)
+	self.__index = self
+	return ext
 end
 
 function Step:apply(ast, pipeline)
 	logger:error("Abstract Steps cannot be Applied")
 end
 
-Step.Name = "Abstract Step";
-Step.Description = "Abstract Step";
-
-return Step;
+return Step
