f1eb12b5b91398185142a980723bf5e9ff05a01d
RileyDoesCode <enriquejoaquingreyes11@gmail.com>
2026-03-06 14:50:15 +0800

    Update highlightlua.lua

--- Prometheus/src/highlightlua.lua
+++ Prometheus/src/highlightlua.lua
@@ -1,20 +1,26 @@
 -- This Script is Part of the Prometheus Obfuscator by Levno_710
 --
--- This Script provides a simple Method for Syntax Highlighting of Lua code
+-- Provides a simple method for syntax highlighting Lua code
 
-local Tokenizer = require("prometheus.tokenizer");
-local colors    = require("colors");
-local TokenKind = Tokenizer.TokenKind;
-local lookupify = require("prometheus.util").lookupify;
+local Tokenizer = require("prometheus.tokenizer")
+local colors    = require("colors")
+
+local TokenKind = Tokenizer.TokenKind
+local lookupify = require("prometheus.util").lookupify
 
 return function(code, luaVersion)
-    local out = "";
-    local tokenizer = Tokenizer:new({
-        LuaVersion = luaVersion,
-    });
+    local out = ""
 
-    tokenizer:append(code);
-    local tokens = tokenizer:scanAll();
+    local tokenizer = Tokenizer:new({
+        LuaVersion = luaVersion
+    })
+
+    tokenizer:append(code)
+    local tokens = tokenizer:scanAll()
+
+    --------------------------------------------------
+    -- Lookup tables
+    --------------------------------------------------
 
     local nonColorSymbols = lookupify{
         ",", ";", "(", ")", "{", "}", ".", ":", "[", "]"
@@ -24,38 +30,51 @@ return function(code, luaVersion)
         "string", "table", "bit32", "bit"
     }
 
-    local currentPos = 1;
+    --------------------------------------------------
+    -- Token processing
+    --------------------------------------------------
+
+    local currentPos = 1
+
     for _, token in ipairs(tokens) do
         if token.startPos >= currentPos then
-            out = out .. string.sub(code, currentPos, token.startPos);
-        end
-        if token.kind == TokenKind.Ident then
-            if defaultGlobals[token.source] then
-                out = out .. colors(token.source, "red");
-            else
-                out = out .. token.source;
-            end
-        elseif token.kind == TokenKind.Keyword then
-            if token.source == "nil" then
-                out = out .. colors(token.source, "yellow");
-            else
-                out = out .. colors(token.source, "yellow");
-            end
-        elseif token.kind == TokenKind.Symbol then
-            if nonColorSymbols[token.source] then
-                out = out .. token.source;
-            else
-                out = out .. colors(token.source, "yellow");
-            end
-        elseif token.kind == TokenKind.String then
-            out = out .. colors(token.source, "green")
-        elseif token.kind == TokenKind.Number then
-            out = out .. colors(token.source, "red")
-        else
-            out = out .. token.source;
+            out = out .. string.sub(code, currentPos, token.startPos)
         end
 
-        currentPos = token.endPos + 1;
+        if token.kind == TokenKind.Ident then
+
+            if defaultGlobals[token.source] then
+                out = out .. colors(token.source, "red")
+            else
+                out = out .. token.source
+            end
+
+        elseif token.kind == TokenKind.Keyword then
+
+            out = out .. colors(token.source, "yellow")
+
+        elseif token.kind == TokenKind.Symbol then
+
+            if nonColorSymbols[token.source] then
+                out = out .. token.source
+            else
+                out = out .. colors(token.source, "yellow")
+            end
+
+        elseif token.kind == TokenKind.String then
+
+            out = out .. colors(token.source, "green")
+
+        elseif token.kind == TokenKind.Number then
+
+            out = out .. colors(token.source, "red")
+
+        else
+            out = out .. token.source
+        end
+
+        currentPos = token.endPos + 1
     end
-    return out;
+
+    return out
 end
