03bb71b91e68a97834aca223289e064314c0cb8a
RileyDoesCode <enriquejoaquingreyes11@gmail.com>
2026-03-06 15:29:17 +0800

    Update enums.lua

--- Prometheus/src/prometheus/enums.lua
+++ Prometheus/src/prometheus/enums.lua
@@ -3,104 +3,112 @@
 -- enums.lua
 -- This file Provides some enums used by the Obfuscator
 
-local Enums = {};
+local chararray = require("prometheus.util").chararray
 
-local chararray = require("prometheus.util").chararray;
+local Enums = {}
 
 Enums.LuaVersion = {
-	LuaU  = "LuaU" ,
+	LuaU  = "LuaU",
 	Lua51 = "Lua51",
+}
+
+-- Cache shared constant tables to reduce memory footprint and module loading time
+local sharedSymbolChars       = chararray("+-*/%^#=~<>(){}[];:,.")
+local sharedIdentChars        = chararray("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_0123456789")
+local sharedNumberChars       = chararray("0123456789")
+local sharedHexNumberChars    = chararray("0123456789abcdefABCDEF")
+local sharedBinaryNumberChars = {"0", "1"}
+local sharedDecimalExponent   = {"e", "E"}
+local sharedHexadecimalNums   = {"x", "X"}
+local sharedBinaryNums        = {"b", "B"}
+
+local sharedEscapeSequences = {
+	["a"]  = "\a",
+	["b"]  = "\b",
+	["f"]  = "\f",
+	["n"]  = "\n",
+	["r"]  = "\r",
+	["t"]  = "\t",
+	["v"]  = "\v",
+	["\\"] = "\\",
+	["\""] = "\"",
+	["\'"] = "\'",
+}
+
+-- Version-specific definitions
+local lua51Keywords = {
+	"and",    "break",  "do",    "else",     "elseif", 
+	"end",    "false",  "for",   "function", "if",   
+	"in",     "local",  "nil",   "not",      "or",
+	"repeat", "return", "then",  "true",     "until",    "while"
+}
+
+local lua51Symbols = {
+	"+",  "-",  "*",  "/",  "%",  "^",  "#",
+	"==", "~=", "<=", ">=", "<",  ">",  "=",
+	"(",  ")",  "{",  "}",  "[",  "]",
+	";",  ":",  ",",  ".",  "..", "...",
+}
+
+local luauKeywords = {
+	"and",    "break",  "do",    "else",     "elseif", "continue",
+	"end",    "false",  "for",   "function", "if",   
+	"in",     "local",  "nil",   "not",      "or",
+	"repeat", "return", "then",  "true",     "until",    "while"
+}
+
+local luauSymbols = {
+	"+",  "-",  "*",  "/",  "%",  "^",  "#",
+	"==", "~=", "<=", ">=", "<",  ">",  "=",
+	"+=", "-=", "/=", "%=", "^=", "..=", "*=",
+	"(",  ")",  "{",  "}",  "[",  "]",
+	";",  ":",  ",",  ".",  "..", "...",
+	"::", "->", "?",  "|",  "&", 
 }
 
 Enums.Conventions = {
 	[Enums.LuaVersion.Lua51] = {
-		Keywords = {
-			"and",    "break",  "do",    "else",     "elseif", 
-			"end",    "false",  "for",   "function", "if",   
-			"in",     "local",  "nil",   "not",      "or",
-			"repeat", "return", "then",  "true",     "until",    "while"
-		},
-		
-		SymbolChars = chararray("+-*/%^#=~<>(){}[];:,."),
-		MaxSymbolLength = 3,
-		Symbols = {
-			"+",  "-",  "*",  "/",  "%",  "^",  "#",
-			"==", "~=", "<=", ">=", "<",  ">",  "=",
-			"(",  ")",  "{",  "}",  "[",  "]",
-			";",  ":",  ",",  ".",  "..", "...",
-		},
+		Keywords                    = lua51Keywords,
+		SymbolChars                 = sharedSymbolChars,
+		MaxSymbolLength             = 3,
+		Symbols                     = lua51Symbols,
 
-		IdentChars          = chararray("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_0123456789"),
-		NumberChars         = chararray("0123456789"),
-		HexNumberChars      = chararray("0123456789abcdefABCDEF"),
-		BinaryNumberChars   = {"0", "1"},
-		DecimalExponent     = {"e", "E"},
-		HexadecimalNums     = {"x", "X"},
-		BinaryNums          = {"b", "B"},
-		DecimalSeperators   = false,
+		IdentChars                  = sharedIdentChars,
+		NumberChars                 = sharedNumberChars,
+		HexNumberChars              = sharedHexNumberChars,
+		BinaryNumberChars           = sharedBinaryNumberChars,
+		DecimalExponent             = sharedDecimalExponent,
+		HexadecimalNums             = sharedHexadecimalNums,
+		BinaryNums                  = sharedBinaryNums,
+		DecimalSeperators           = false,
 		
-		EscapeSequences     = {
-			["a"] = "\a";
-			["b"] = "\b";
-			["f"] = "\f";
-			["n"] = "\n";
-			["r"] = "\r";
-			["t"] = "\t";
-			["v"] = "\v";
-			["\\"] = "\\";
-			["\""] = "\"";
-			["\'"] = "\'";
-		},
-		NumericalEscapes = true,
+		EscapeSequences             = sharedEscapeSequences,
+		NumericalEscapes            = true,
 		EscapeZIgnoreNextWhitespace = true,
-		HexEscapes = true,
-		UnicodeEscapes = true,
+		HexEscapes                  = true,
+		UnicodeEscapes              = true,
 	},
 	[Enums.LuaVersion.LuaU] = {
-		Keywords = {
-			"and",    "break",  "do",    "else",     "elseif", "continue",
-			"end",    "false",  "for",   "function", "if",   
-			"in",     "local",  "nil",   "not",      "or",
-			"repeat", "return", "then",  "true",     "until",    "while"
-		},
-		
-		SymbolChars = chararray("+-*/%^#=~<>(){}[];:,."),
-		MaxSymbolLength = 3,
-		Symbols = {
-			"+",  "-",  "*",  "/",  "%",  "^",  "#",
-			"==", "~=", "<=", ">=", "<",  ">",  "=",
-			"+=", "-=", "/=", "%=", "^=", "..=", "*=",
-			"(",  ")",  "{",  "}",  "[",  "]",
-			";",  ":",  ",",  ".",  "..", "...",
-			"::", "->", "?",  "|",  "&", 
-		},
+		Keywords                    = luauKeywords,
+		SymbolChars                 = sharedSymbolChars,
+		MaxSymbolLength             = 3,
+		Symbols                     = luauSymbols,
 
-		IdentChars          = chararray("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_0123456789"),
-		NumberChars         = chararray("0123456789"),
-		HexNumberChars      = chararray("0123456789abcdefABCDEF"),
-		BinaryNumberChars   = {"0", "1"},
-		DecimalExponent     = {"e", "E"},
-		HexadecimalNums     = {"x", "X"},
-		BinaryNums          = {"b", "B"},
-		DecimalSeperators   = {"_"},
+		IdentChars                  = sharedIdentChars,
+		NumberChars                 = sharedNumberChars,
+		HexNumberChars              = sharedHexNumberChars,
+		BinaryNumberChars           = sharedBinaryNumberChars,
+		DecimalExponent             = sharedDecimalExponent,
+		HexadecimalNums             = sharedHexadecimalNums,
+		BinaryNums                  = sharedBinaryNums,
+		DecimalSeperators           = {"_"},
 		
-		EscapeSequences     = {
-			["a"] = "\a";
-			["b"] = "\b";
-			["f"] = "\f";
-			["n"] = "\n";
-			["r"] = "\r";
-			["t"] = "\t";
-			["v"] = "\v";
-			["\\"] = "\\";
-			["\""] = "\"";
-			["\'"] = "\'";
-		},
-		NumericalEscapes = true,
+		EscapeSequences             = sharedEscapeSequences,
+		NumericalEscapes            = true,
 		EscapeZIgnoreNextWhitespace = true,
-		HexEscapes = true,
-		UnicodeEscapes = true,
+		HexEscapes                  = true,
+		UnicodeEscapes              = true,
 	},
 }
 
-return Enums;
+return Enums
