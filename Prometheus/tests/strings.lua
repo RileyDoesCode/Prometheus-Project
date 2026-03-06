177bb15ea2719fb139cf05c746bd7617e0e7e3e0
RileyDoesCode <enriquejoaquingreyes11@gmail.com>
2026-03-06 14:17:23 +0800

    Update strings.lua

--- Prometheus/tests/strings.lua
+++ Prometheus/tests/strings.lua
@@ -1,12 +1,27 @@
 -- Deterministic text statistics for repeated words
+
+--------------------------------------------------
+-- Input text
+--------------------------------------------------
+
 local passage = "lorem ipsum dolor sit amet ipsum lorem"
+
+--------------------------------------------------
+-- Word counting
+--------------------------------------------------
+
 local counts = {}
 
 for word in passage:gmatch("%w+") do
     counts[word] = (counts[word] or 0) + 1
 end
 
-local order = {"lorem", "ipsum", "dolor", "sit", "amet"}
+--------------------------------------------------
+-- Deterministic output order
+--------------------------------------------------
+
+local order = { "lorem", "ipsum", "dolor", "sit", "amet" }
+
 for _, word in ipairs(order) do
     print(string.format("%s:%d", word, counts[word] or 0))
 end
