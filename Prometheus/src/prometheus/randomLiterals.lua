63a457d6e6ae1e84521b850dc33409fc2253efe6
RileyDoesCode <enriquejoaquingreyes11@gmail.com>
2026-03-06 15:36:36 +0800

    Update randomLiterals.lua

--- Prometheus/src/prometheus/randomLiterals.lua
+++ Prometheus/src/prometheus/randomLiterals.lua
@@ -2,40 +2,37 @@
 --
 -- Library for Creating Random Literals
 
-local Ast = require("prometheus.ast");
-local RandomStrings = require("prometheus.randomStrings");
+local Ast = require("prometheus.ast")
+local RandomStrings = require("prometheus.randomStrings")
 
-local RandomLiterals = {};
+local RandomLiterals = {}
 
-local function callNameGenerator(generatorFunction, ...)
-	if(type(generatorFunction) == "table") then
-		generatorFunction = generatorFunction.generateName;
-	end
-	return generatorFunction(...);
+local function callNameGenerator(generator, ...)
+	local func = type(generator) == "table" and generator.generateName or generator
+	return func(...)
 end
 
 function RandomLiterals.String(pipeline)
-    return Ast.StringExpression(callNameGenerator(pipeline.namegenerator, math.random(1, 4096)));
+	return Ast.StringExpression(callNameGenerator(pipeline.namegenerator, math.random(1, 4096)))
 end
 
 function RandomLiterals.Dictionary()
-    return RandomStrings.randomStringNode(true);
+	return RandomStrings.randomStringNode(true)
 end
 
 function RandomLiterals.Number()
-    return Ast.NumberExpression(math.random(-8388608, 8388607));
+	return Ast.NumberExpression(math.random(-8388608, 8388607))
 end
 
 function RandomLiterals.Any(pipeline)
-    local type = math.random(1, 3);
-    if type == 1 then
-        return RandomLiterals.String(pipeline);
-    elseif type == 2 then
-        return RandomLiterals.Number();
-    elseif type == 3 then
-        return RandomLiterals.Dictionary();
-    end
+	local choice = math.random(1, 3)
+	if choice == 1 then
+		return RandomLiterals.String(pipeline)
+	elseif choice == 2 then
+		return RandomLiterals.Number()
+	else
+		return RandomLiterals.Dictionary()
+	end
 end
 
-
-return RandomLiterals;
+return RandomLiterals
