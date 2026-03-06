cc092fba864077d1a12374476b91bb879982d8d8
RileyDoesCode <enriquejoaquingreyes11@gmail.com>
2026-03-06 16:37:07 +0800

    Update mangled_shuffled.lua

--- Prometheus/src/prometheus/namegenerators/mangled_shuffled.lua
+++ Prometheus/src/prometheus/namegenerators/mangled_shuffled.lua
@@ -2,34 +2,40 @@
 --
 -- namegenerators/mangled_shuffled.lua
 --
--- This Script provides a function for generation of mangled names with shuffled character order
+-- This Script provides a function for the generation of mangled names with shuffled character order
 
+local util = require("prometheus.util")
+local chararray = util.chararray
 
-local util = require("prometheus.util");
-local chararray = util.chararray;
+local VarDigits = chararray("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_")
+local VarStartDigits = chararray("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
-local VarDigits = chararray("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_");
-local VarStartDigits = chararray("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
+-- Cache lengths globally since shuffling changes the order, but not the size
+local startLen = #VarStartDigits
+local digitsLen = #VarDigits
 
 local function generateName(id, scope)
-	local name = ''
-	local d = id % #VarStartDigits
-	id = (id - d) / #VarStartDigits
-	name = name..VarStartDigits[d+1]
+	local d = id % startLen
+	id = (id - d) / startLen
+	
+	-- Initialize table with the starting character
+	local result = { VarStartDigits[d + 1] }
+	
 	while id > 0 do
-		local d = id % #VarDigits
-		id = (id - d) / #VarDigits
-		name = name..VarDigits[d+1]
+		d = id % digitsLen
+		id = (id - d) / digitsLen
+		result[#result + 1] = VarDigits[d + 1]
 	end
-	return name
+	
+	return table.concat(result)
 end
 
 local function prepare(ast)
-	util.shuffle(VarDigits);
-	util.shuffle(VarStartDigits);
+	util.shuffle(VarDigits)
+	util.shuffle(VarStartDigits)
 end
 
 return {
 	generateName = generateName, 
 	prepare = prepare
-};
+}
