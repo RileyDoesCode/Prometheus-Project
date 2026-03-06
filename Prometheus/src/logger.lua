6cd826011e3f7196bd8d792d62c15a039b5d60b0
RileyDoesCode <enriquejoaquingreyes11@gmail.com>
2026-03-06 14:36:22 +0800

    Update logger.lua

--- Prometheus/src/logger.lua
+++ Prometheus/src/logger.lua
@@ -3,60 +3,86 @@
 -- logger.lua
 
 local logger = {}
-local config = require("config");
-local colors = require("colors");
+
+local config = require("config")
+local colors = require("colors")
+
+--------------------------------------------------
+-- Log Levels
+--------------------------------------------------
 
 logger.LogLevel = {
-	Error = 0,
-	Warn = 1,
-	Log = 2,
-	Info = 2,
-	Debug = 3,
+    Error = 0,
+    Warn  = 1,
+    Log   = 2,
+    Info  = 2,
+    Debug = 3
 }
 
-logger.logLevel = logger.LogLevel.Log;
+logger.logLevel = logger.LogLevel.Log
+
+--------------------------------------------------
+-- Debug
+--------------------------------------------------
 
 logger.debugCallback = function(...)
-	print(colors(config.NameUpper .. ": " ..  ..., "grey"));
-end;
-function logger:debug(...)
-	if self.logLevel >= self.LogLevel.Debug then
-		self.debugCallback(...);
-	end
+    print(colors(config.NameUpper .. ": " .. ..., "grey"))
 end
 
+function logger:debug(...)
+    if self.logLevel >= self.LogLevel.Debug then
+        self.debugCallback(...)
+    end
+end
+
+--------------------------------------------------
+-- Log / Info
+--------------------------------------------------
+
 logger.logCallback = function(...)
-	print(colors(config.NameUpper .. ": ", "magenta") .. ...);
-end;
+    print(colors(config.NameUpper .. ": ", "magenta") .. ...)
+end
+
 function logger:log(...)
-	if self.logLevel >= self.LogLevel.Log then
-		self.logCallback(...);
-	end
+    if self.logLevel >= self.LogLevel.Log then
+        self.logCallback(...)
+    end
 end
 
 function logger:info(...)
-	if self.logLevel >= self.LogLevel.Log then
-		self.logCallback(...);
-	end
+    if self.logLevel >= self.LogLevel.Log then
+        self.logCallback(...)
+    end
 end
+
+--------------------------------------------------
+-- Warning
+--------------------------------------------------
 
 logger.warnCallback = function(...)
-	print(colors(config.NameUpper .. ": " .. ..., "yellow"));
-end;
-function logger:warn(...)
-	if self.logLevel >= self.LogLevel.Warn then
-		self.warnCallback(...);
-	end
+    print(colors(config.NameUpper .. ": " .. ..., "yellow"))
 end
+
+function logger:warn(...)
+    if self.logLevel >= self.LogLevel.Warn then
+        self.warnCallback(...)
+    end
+end
+
+--------------------------------------------------
+-- Error
+--------------------------------------------------
 
 logger.errorCallback = function(...)
-	print(colors(config.NameUpper .. ": " .. ..., "red"))
-	error(...);
-end;
-function logger:error(...)
-	self.errorCallback(...);
-	error(config.NameUpper .. ": logger.errorCallback did not throw an Error!");
+    print(colors(config.NameUpper .. ": " .. ..., "red"))
+    error(...)
 end
 
+function logger:error(...)
+    self.errorCallback(...)
+    error(config.NameUpper .. ": logger.errorCallback did not throw an Error!")
+end
 
-return logger;
+--------------------------------------------------
+
+return logger
