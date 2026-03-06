91b39abc539e7dd14343b8fdc6e06f2ea37d1754
RileyDoesCode <enriquejoaquingreyes11@gmail.com>
2026-03-06 15:37:51 +0800

    Update randomStrings.lua

--- Prometheus/src/prometheus/randomStrings.lua
+++ Prometheus/src/prometheus/randomStrings.lua
@@ -1,18 +1,27 @@
+-- This Script is Part of the Prometheus Obfuscator by Levno_710
+--
+-- randomStrings.lua
+
 local Ast = require("prometheus.ast")
 local utils = require("prometheus.util")
+
 local charset = utils.chararray("qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890")
+local charsetLen = #charset
 
 local function randomString(wordsOrLen)
 	if type(wordsOrLen) == "table" then
-		return wordsOrLen[math.random(1, #wordsOrLen)];
+		return wordsOrLen[math.random(1, #wordsOrLen)]
 	end
 
-	wordsOrLen = wordsOrLen or math.random(2, 15);
-	if wordsOrLen > 0 then
-		return randomString(wordsOrLen - 1) .. charset[math.random(1, #charset)]
-	else
-		return ""
+	local len = wordsOrLen or math.random(2, 15)
+	
+	-- Use a table to build the string iteratively for significantly better performance
+	local result = {}
+	for i = 1, len do
+		result[i] = charset[math.random(1, charsetLen)]
 	end
+	
+	return table.concat(result)
 end
 
 local function randomStringNode(wordsOrLen)
